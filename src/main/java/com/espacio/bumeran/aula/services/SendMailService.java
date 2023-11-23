f6881b3ea9d6dca08c272b5f1905f70aaf3eef46
6835938 <jsanchez.lopez@renfe.es>
2023-11-24 00:22:04 +0100

    Cambio en sender correo

---
+++ src/main/java/com/espacio/bumeran/aula/services/SendMailService.java
@@ -0,0 +1,41 @@
+package com.espacio.bumeran.aula.services;
+
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SendMailService {
+	
+	//@Autowired
+	//private JavaMailSender javaMailSender;
+	
+	public void sendMail (String from, String to, String subject, String body) {
+		JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+		javaMailSender.setHost("smtp.ionos.es");
+		javaMailSender.setPort(587);
+	    
+		javaMailSender.setUsername("info@espaciobumeran.com");
+		javaMailSender.setPassword("DavidSanchez_2019");
+	    
+	    Properties props = javaMailSender.getJavaMailProperties();
+	    props.put("mail.transport.protocol", "smtp");
+	    props.put("mail.smtp.auth", "true");
+	    props.put("mail.smtp.starttls.enable", "true");
+	    
+	    
+		
+		
+		SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+		simpleMailMessage.setFrom(from);
+		simpleMailMessage.setTo(to);
+		simpleMailMessage.setSubject(subject);
+		simpleMailMessage.setText(body);
+		
+		javaMailSender.send(simpleMailMessage);
+	}
+}	
